4bb9190beb0052f586d471e5ba5f5b0bf18e6ef2
John Parnis <john.parnis.13@um.edu.mt>
2019-04-17 18:11:56 +0200

    Setting up map class

---
+++ src/main/java/Map.java
@@ -0,0 +1,25 @@
+public class Map {
+    private int size;
+
+    public Map(){
+
+    }
+
+    public Map(int size){
+        this.size = size;
+    }
+
+    public Boolean setMap(int x, int y){
+
+    }
+
+    public void generatMap(){
+
+    }
+
+    public char getTileTyoe(int x, int y){
+        
+    }
+
+
+}
