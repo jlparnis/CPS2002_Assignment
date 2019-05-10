2881cb91ca20a6f6db9ce7056979c80610beb872
Gerard <gerardtabone@yahoo.com>
2019-05-10 13:20:28 +0200

    Added test for new map types

--- src/test/java/HazardousMapTest.java
+++ src/test/java/HazardousMapTest.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class HazardousMapTest {
+
+    private Map map;
+
+    @Before
+    public void setup() {
+        map = new HazardousMap();
+    }
+
+    @After
+    public void teardown() {
+        map = null;
+    }
+
+    @Test
+    public void test_map_size() {
+        int n = 10;
+        map = new HazardousMap(n);
+        Assert.assertEquals(n, map.getMapSize());
+    }
+
 }
--- src/test/java/SafeMapTest.java
+++ src/test/java/SafeMapTest.java
@@ -1,4 +1,26 @@
-package PACKAGE_NAME;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class SafeMapTest {
+    private Map map;
+
+    @Before
+    public void setup() {
+        map = new SafeMap();
+    }
+
+    @After
+    public void teardown() {
+        map = null;
+    }
+
+    @Test
+    public void test_map_size() {
+        int n = 10;
+        map = new SafeMap(n);
+        Assert.assertEquals(n, map.getMapSize());
+    }
+
 }
